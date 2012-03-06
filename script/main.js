e061e01023c8292522142e393bfcafd3336b0b2e
Todd Anderson <bustardcelly@gmail.com>
2012-03-05 21:29:22 -0500

    Change of contextual naming of facades.

--- script/main.js
+++ script/main.js
@@ -7,7 +7,7 @@
 		require.config({
 			baseUrl: '.'
 		});
-		require( ['script/selector/SimpleSelector', 'script/router/PathHashTagAdapter', 'lib/vine'], handleLibs );
+		require( ['script/selector/SimpleSelectorFacade', 'script/router/PathHashTagFacade', 'lib/vine'], handleLibs );
 	}
 
 	function handleLibs( selectorFacade, routeFacade, nonAMDObserver ) {
